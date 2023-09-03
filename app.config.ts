245eada591311067dba6a247cd0f1aa05317e0cd
Matthew Hinton <mhinton@gmail.com>
2023-09-03 14:09:44 -0500

    css: icon size and change default image

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
     title: 'Alpine',
     description: 'The minimalist blog theme',
     image: {
-      src: '/social-card-preview.png',
+      src: '/logo-dark.svg',
       alt: 'An image showcasing my project.',
       width: 400,
       height: 300
@@ -37,5 +37,9 @@ export default defineAppConfig({
     form: {
       successMessage: 'Message sent. Thank you!'
     }
+  },
+
+  nuxtIcon: {
+    size: '1rem'
   }
 })
